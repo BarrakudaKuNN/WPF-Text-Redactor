b5e74d5e1541e32e0c81b066288d3f8ff0dc61b7
Vladislav <Risovanienasaite@gmail.com>
2023-09-06 13:17:49 +0300

    Created FileDialog. Nothing new

---
+++ Wpf-Text-Redactor/Logic/Connector.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_Text_Redactor.Logic
+{
+    internal class Connector
+    {
+        OpenFileDialog file_Dialog;
+
+        void Dialog_Open()
+        {
+            file_Dialog = new OpenFileDialog();
+            file_Dialog.Filter = "Text files (*.txt)|*.txt";
+            file_Dialog.FilterIndex = 1;
+            file_Dialog.RestoreDirectory = true;
+            Nullable<bool> result = file_Dialog.ShowDialog();
+        }
+        public void Text_Open()
+        {
+            Dialog_Open();
+
+        }
+    }
+}
---
+++ Wpf-Text-Redactor/Logic/Redactor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_Text_Redactor.Logic
+{
+    internal class Redactor 
+    {
+        string file;
+
+        public void Text_Save()
+        {
+
+
+        }
+    }
+}
